4edb60b2d4645e963e8d9cf6c0790ea972bcf948
Aditya Patel <adityapatel@Adityas-MacBook-Air.local>
2023-11-13 10:44:35 -0500

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
             <img src="/introImage.png" alt={"Aditya Patel Profile Picture"} className="introProfileImage border border-5 rounded-circle"/>
         </div>
         <p> 
-            {"Hey, I'm"} <span style={{textDecoration: "underline"}}>{"Aditya Patel"}</span> {", I'm a "} <span style={{textDecoration: "underline"}}>{"Computer Science"}</span> {" undergrad at"} <a href="https://www.torontomu.ca/about/" className="introLink hover-underline-animation">Toronto Metropolitan University</a> {"in my junior year, planning on graduating by December 2025. A passionate programmer, I am eager to challenge myself to do things I’ve never accomplished before and I strive to learn, build and engineer stuff (checkout my "} <Link className="introLink hover-underline-animation" href="./misc">{"Projects"}</Link>{")."}
+            {"Hey, I'm"} <span style={{textDecoration: "underline"}}>{"Aditya Patel"}</span> {", I'm a "} <span style={{textDecoration: "underline"}}>{"Computer Science"}</span> {" undergrad at"} <a href="https://www.torontomu.ca/about/" className="introLink hover-underline-animation">Toronto Metropolitan University</a> {"in my junior year, planning on graduating by December 2025. A passionate programmer, I like challenging myself to do things I’ve never accomplished before and I strive to learn, build and engineer stuff (checkout my "} <Link className="introLink hover-underline-animation" href="./misc">{"Projects"}</Link>{")."}
         </p>
         <p>
             {"Currently a "} <Link style={{textDecoration: "underline"}} className="introLinkJob hover-underline-animation" href="/experience/RBC">{"Software Engineer Intern"}</Link> {" at "} 
@@ -30,7 +30,7 @@ export default function Home() {
             </Link> (Web)  {"for"} <a href="https://www.gbm.scotiabank.com/en.html" className="introLink hover-underline-animation">Scotiabank Capital Markets</a> {" where I engineered low latency applications and I'm open to Internship/Research opportunities."}
         </p>
         <p>
-            {"I'm into Swimming and Basketball, love talking about Space, Physics and about different food options in the Toronto area, I'm also a sucker for a good book or TV Show recommendation - So feel free to shoot me an email - "} <span><a className="hover-underline-animation introLink" href="mailto:aditya.patel@torontomu.ca">aditya.patel@torontomu.ca</a></span> {" , or set-up some time in my schedule by clicking the calendar below!"}
+            {"I'm into Swimming and Basketball, I also love talking about Space, Physics and about different food options here in Toronto - So feel free to shoot me an email - "} <span><a className="hover-underline-animation introLink" href="mailto:aditya.patel@torontomu.ca">aditya.patel@torontomu.ca</a></span> {" , or set-up some time in my schedule by clicking the calendar below!"}
         </p>
         <div className="introSocials">
             <Link href="https://www.linkedin.com/in/thisisadityapatel/" className="introSocialIconLink" target="_blank"><i className="bi bi-linkedin introIconFont"></i></Link>
